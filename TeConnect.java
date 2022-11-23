4d615433d2cafdd435889a0736a7f7631c141c39
Dystractor <38833077+Dystractor@users.noreply.github.com>
2022-11-23 18:31:28 +0800

    Update TeConnect.java

--- TeConnect.java
+++ TeConnect.java
@@ -4,15 +4,15 @@ import java.io.Closeable;
 import java.io.IOException;
 
 public class TeConnect {
-    public static void close(Closeable ... connections) {
-        for (Closeable Connect : connections) {
-            if(Connect != null) {
-                try {
-                    Connect.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+  public static void close(Closeable ... connections) {
+    for (Closeable Connect : connections) {
+      if(Connect != null) {
+        try {
+          Connect.close();
+        } catch (IOException e) {
+          e.printStackTrace();
         }
+      }
     }
+  }
 }
