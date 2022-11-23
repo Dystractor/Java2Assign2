ca17ad466cb4e897d02fa4487a134ecccb8adb52
Dystractor <38833077+Dystractor@users.noreply.github.com>
2022-11-23 19:49:45 +0800

    Update constance.java

--- constance.java
+++ constance.java
@@ -1,10 +1,10 @@
 package application.pane;
 
 public interface constance {
-    int player1 = 1;
-    int player2 = 2;
-    int player1_win = 3;
-    int player2_win = 4;
-    int draw = 5;
-    int Continue = 6;
+  int player1 = 1;
+  int player2 = 2;
+  int player1_win = 3;
+  int player2_win = 4;
+  int draw = 5;
+  int Continue = 6;
 }
